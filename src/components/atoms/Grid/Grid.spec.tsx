81e9734ad3c88c0a8d94ad5e3ef2806e5d466746
Luiz Henrique <luizhjramos@outlook.com>
2023-07-19 23:31:29 -0300

    test: ensure be able to render the grid correctly

---
+++ src/components/atoms/Grid/Grid.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import { Grid } from '.'
+
+describe('Components > Atoms - Grid', () => {
+  it('should be able to render the grid correctly', () => {
+    render(<Grid>Grid</Grid>)
+
+    expect(screen.getByText(/grid/i)).toBeVisible()
+  })
+})

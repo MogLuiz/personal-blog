db0c785a5796ed8c9f2d80163565cfa316f9b624
Luiz Henrique <luizhjramos@outlook.com>
2023-07-19 23:31:50 -0300

    test: ensure be able to render the grid with the correct number of columns

--- src/components/atoms/Grid/Grid.spec.tsx
+++ src/components/atoms/Grid/Grid.spec.tsx
@@ -8,4 +8,10 @@ describe('Components > Atoms - Grid', () => {
 
     expect(screen.getByText(/grid/i)).toBeVisible()
   })
+
+  it('should be able to render the grid with the correct number of columns', () => {
+    render(<Grid cols={3}>Grid</Grid>)
+
+    expect(screen.getByText(/grid/i)).toHaveClass('grid-cols-3')
+  })
 })

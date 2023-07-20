353f467c2d10a79ef9b5e39705ac69503acac198
Luiz Henrique <luizhjramos@outlook.com>
2023-07-20 13:25:02 -0300

    refactor: abstract query logic to setup test helper

--- src/components/atoms/Profile/Profile.spec.tsx
+++ src/components/atoms/Profile/Profile.spec.tsx
@@ -2,21 +2,26 @@ import { render, screen } from '@testing-library/react'
 
 import { Profile } from '.'
 
+const factorySetupTest = () => {
+  render(<Profile title="John Doe" subtitle="Software Engineer" />)
+
+  const profileImage = screen.getByRole('img')
+  const profileTitle = screen.getByText(/john doe/i)
+  const profileSubtitle = screen.getByText(/software engineer/i)
+
+  return { profileSubtitle, profileTitle, profileImage }
+}
+
 describe('Components > Atoms - Profile', () => {
   it('should be able to render the profile correctly', () => {
-    render(<Profile title="John Doe" subtitle="Software Engineer" />)
-
-    const profileTitle = screen.getByText(/john doe/i)
-    const profileSubtitle = screen.getByText(/software engineer/i)
+    const { profileSubtitle, profileTitle } = factorySetupTest()
 
     expect(profileTitle).toBeVisible()
     expect(profileSubtitle).toBeVisible()
   })
 
   it('should be able to render the profile image correctly', () => {
-    render(<Profile title="John Doe" subtitle="Software Engineer" />)
-
-    const profileImage = screen.getByRole('img')
+    const { profileImage } = factorySetupTest()
 
     expect(profileImage).toBeVisible()
     expect(profileImage).toHaveAttribute('alt', 'Profile image')

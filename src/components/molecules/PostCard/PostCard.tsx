db0e34e978c8b17d034ebc980e501542c88560ac
Luiz Henrique <luizhjramos@outlook.com>
2023-07-29 17:46:30 -0300

    refactor: move styles from jsx component to own styles file

--- src/components/molecules/PostCard/PostCard.tsx
+++ src/components/molecules/PostCard/PostCard.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { Tag } from '@/components/atoms/Tag'
 
 import { BlogPost } from '@/models'
+
+import * as S from './styles'
 
 export const PostCard = ({
   readingTime,
@@ -14,31 +15,29 @@ export const PostCard = ({
   title,
   tags
 }: BlogPost) => (
-  <Link href={slug}>
-    <div className="relative h-80 w-full">
+  <S.LinkContainer href={slug}>
+    <S.ImageContainer>
       <Image
         src={image}
         fill
-        alt="title"
+        alt={title}
         priority
         className="rounded-xl object-cover object-center"
       />
-    </div>
+    </S.ImageContainer>
 
-    <div className="pt-3">
-      <div className="mb-3 flex flex-wrap gap-2">
+    <S.Content>
+      <S.TagsContainer>
         {tags?.map((tag) => <Tag key={tag}>{tag}</Tag>)}
-      </div>
+      </S.TagsContainer>
 
-      <time className="text-gray-400">
+      <S.Time>
         {date} • {readingTime} minutos de leitura
-      </time>
+      </S.Time>
 
-      <h3 className="mt-2 max-w-md text-ellipsis text-2xl font-medium text-gray-50">
-        {title}
-      </h3>
+      <S.Title>{title}</S.Title>
 
-      <p className="mt-3 text-gray-400">{description}</p>
-    </div>
-  </Link>
+      <S.Description>{description}</S.Description>
+    </S.Content>
+  </S.LinkContainer>
 )

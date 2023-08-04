9006e2bd0136884b244607abca2faed4f5c1017a
Luiz Henrique <luizhjramos@outlook.com>
2023-08-04 08:08:03 -0300

    feat: add link to back home in the logo image

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Logo = () => (
-  <Image
-    aria-label="Logo do projeto"
-    className="pb-6 pt-6"
-    src="/assets/images/logo.svg"
-    alt="Logo"
-    width={220}
-    height={340}
-  />
+  <Link href="/">
+    <Image
+      aria-label="Logo do projeto"
+      className="pb-6 pt-6"
+      src="/assets/images/logo.svg"
+      alt="Logo"
+      width={220}
+      height={340}
+    />
+  </Link>
 )

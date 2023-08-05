cbf760db83a64ec8a8f2e5345f8e7c4240ea2d65
Luiz Henrique <luizhjramos@outlook.com>
2023-08-05 11:06:32 -0300

    feat: add BackButton JSX structure

--- src/components/atoms/BackToTop/BackToTop.tsx
+++ src/components/atoms/BackToTop/BackToTop.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import * as S from './styles'
+
 export const BackToTop = () => {
   return (
-    <div>
-      <button>Back to top</button>
-    </div>
+    <S.Container>
+      <S.Button
+        title="Voltar ao topo"
+        aria-label="Voltar ao topo"
+        onClick={() => window.scrollTo(0, 0)}
+      >
+        <S.Icon />
+      </S.Button>
+    </S.Container>
   )
 }

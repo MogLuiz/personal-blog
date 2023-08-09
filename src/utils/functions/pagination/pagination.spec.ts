f84217e49f614a0175f24e6aeb26fcab32d05fb7
Luiz Henrique <luizhjramos@outlook.com>
2023-08-08 23:39:01 -0300

    test: ensure return paginated data

---
+++ src/utils/functions/pagination/pagination.spec.ts
@@ -0,0 +1,36 @@
+import { dataPagination } from './pagination'
+
+const data = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Doe' },
+  { id: 3, name: 'Jane' },
+  { id: 4, name: 'Jack' },
+  { id: 5, name: 'Jill' }
+]
+
+describe('dataPagination - utility function', () => {
+  it('should return paginated data', () => {
+    const { paginatedData, totalPages } = dataPagination(data, 1, 2)
+
+    expect(paginatedData).toEqual([
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' }
+    ])
+    expect(totalPages).toBe(3)
+
+    const { paginatedData: paginatedData2, totalPages: totalPages2 } =
+      dataPagination(data, 2, 2)
+
+    expect(paginatedData2).toEqual([
+      { id: 3, name: 'Jane' },
+      { id: 4, name: 'Jack' }
+    ])
+    expect(totalPages2).toBe(3)
+
+    const { paginatedData: paginatedData3, totalPages: totalPages3 } =
+      dataPagination(data, 3, 2)
+
+    expect(paginatedData3).toEqual([{ id: 5, name: 'Jill' }])
+    expect(totalPages3).toBe(3)
+  })
+})

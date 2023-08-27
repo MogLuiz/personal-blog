b635b0a30a29310cbeb7356b55e0ba6734299724
Luiz Henrique <luizhjramos@outlook.com>
2023-08-27 14:39:17 -0300

    styles: Link component

--- src/components/atoms/Link/Link.tsx
+++ src/components/atoms/Link/Link.tsx
@@ -1,11 +1,13 @@
 import { AnchorHTMLAttributes } from 'react'
 
+import * as S from './styles'
+
 type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
   children: React.ReactNode
 }
 
 export const Link = ({ children, ...rest }: LinkProps) => (
-  <a {...rest}>
-    <span>{children}</span>
-  </a>
+  <S.Container {...rest}>
+    <S.Content>{children}</S.Content>
+  </S.Container>
 )

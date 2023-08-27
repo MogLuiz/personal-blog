20fd9bcf7820dacfcf146b47d1d0f1d78cd9eda9
Luiz Henrique <luizhjramos@outlook.com>
2023-08-27 12:25:54 -0300

    styles: about screen

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -1,3 +1,35 @@
+import * as S from './styles'
+
 export const AboutUILayer = () => {
-  return <main></main>
+  return (
+    <main>
+      <S.Title>Sobre mim</S.Title>
+
+      <S.Subtitle>Primeiramente, agradeço por estar aqui.</S.Subtitle>
+
+      <S.Paragraph>
+        Sou apaixonado por tecnologia e em como ela transforma a vida das
+        pessoas. Adoro desafios que me tiram da zona de conforto e estou sempre
+        em busca de evoluir tecnicamente.
+      </S.Paragraph>
+
+      <S.Paragraph>
+        Sou apaixonado por tecnologia e em como ela transforma a vida das
+        pessoas. Adoro desafios que me tiram da zona de conforto e estou sempre
+        em busca de evoluir tecnicamente.
+      </S.Paragraph>
+
+      <S.Paragraph>
+        Sou apaixonado por tecnologia e em como ela transforma a vida das
+        pessoas. Adoro desafios que me tiram da zona de conforto e estou sempre
+        em busca de evoluir tecnicamente.
+      </S.Paragraph>
+
+      <S.Paragraph>
+        Sou apaixonado por tecnologia e em como ela transforma a vida das
+        pessoas. Adoro desafios que me tiram da zona de conforto e estou sempre
+        em busca de evoluir tecnicamente.
+      </S.Paragraph>
+    </main>
+  )
 }
---
+++ src/screens/About/styles.ts
@@ -0,0 +1,22 @@
+import tw from 'tailwind-styled-components'
+
+export const Title = tw.h1`
+  mb-6
+  text-3xl
+  font-bold
+`
+
+export const Subtitle = tw.strong`
+  flex
+  mb-4
+  text-xl
+  font-bold
+  leading-relaxed
+`
+
+export const Paragraph = tw.p`
+  mb-2
+  text-xl
+  font-normal
+  leading-relaxed
+`

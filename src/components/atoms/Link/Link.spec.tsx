38d28b4229bb8018049a6d1a0a65e65201106ffb
Luiz Henrique <luizhjramos@outlook.com>
2023-08-27 14:46:38 -0300

    test: ensure be able to render the link with the correct href

--- src/components/atoms/Link/Link.spec.tsx
+++ src/components/atoms/Link/Link.spec.tsx
@@ -8,4 +8,14 @@ describe('Components > Atoms - Link', () => {
 
     expect(screen.getByText(/link/i)).toBeVisible()
   })
+
+  it('should be able to render the link with the correct href', () => {
+    const linkTo = '/about'
+    render(<Link href={linkTo}>Link</Link>)
+
+    expect(screen.getByRole('link', { name: /link/i })).toHaveAttribute(
+      'href',
+      linkTo
+    )
+  })
 })

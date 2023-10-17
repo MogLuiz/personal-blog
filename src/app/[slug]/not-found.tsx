88c1c0a9dbe0463b908ac53d8875086717046478
Luiz Henrique <luizhjramos@outlook.com>
2023-10-16 22:37:38 -0300

    feat: add not-found post page

--- src/app/[slug]/not-found.tsx
+++ src/app/[slug]/not-found.tsx
@@ -1,3 +1,16 @@
+import { Empty } from '@/components/atoms/Empty'
+import { RecommendedPosts } from '@/components/molecules/RecommendedPosts'
+
 export default function NotFound() {
-  return <div>Not Found</div>
+  return (
+    <Empty>
+      <h1 className="text-center text-4xl font-extrabold">
+        Oops. Essa página não foi encontrada.
+      </h1>
+      <p className="mt-4 text-center text-xl text-gray-400">
+        Posts recomendados para você:
+      </p>
+      <RecommendedPosts />
+    </Empty>
+  )
 }

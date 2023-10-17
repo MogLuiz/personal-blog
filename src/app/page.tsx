bd46ae50e15681f2aeb29db803c43fa740d7c7bd
Luiz Henrique <luizhjramos@outlook.com>
2023-10-17 06:15:45 -0300

    feat: add SEO on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next'
+
 import { HomeScreen } from '@/screens/Home'
 import { useListPosts } from '@/services/PostService'
+
+export const metadata: Metadata = {
+  title: 'Home | Luiz Ramos',
+  description: 'Home page'
+}
 
 export default function HomeDataLayer() {
   const data = useListPosts()

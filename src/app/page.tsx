0d4cf0982c6a1bace8767ed94ecc54db89982ed0
Luiz Henrique <luizhjramos@outlook.com>
2023-10-17 06:24:17 -0300

    feat: improve SEO infos on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 import { Metadata } from 'next'
 
+import { siteConfig } from '@/config'
 import { HomeScreen } from '@/screens/Home'
 import { useListPosts } from '@/services/PostService'
-
-export const metadata: Metadata = {
-  title: 'Home | Luiz Ramos',
-  description: 'Home page'
-}
 
 export default function HomeDataLayer() {
   const data = useListPosts()
 
   return <HomeScreen {...data} />
+}
+
+export const metadata: Metadata = {
+  title: 'Home | Luiz Ramos',
+  description: siteConfig.description,
+  metadataBase: new URL(siteConfig.url),
+  openGraph: {
+    title: 'Home | Luiz Ramos',
+    description: siteConfig.description,
+    url: new URL(siteConfig.url),
+    type: 'website',
+    images: [
+      {
+        url: '/images/cover.png',
+        alt: 'Blog image'
+      }
+    ]
+  }
 }
